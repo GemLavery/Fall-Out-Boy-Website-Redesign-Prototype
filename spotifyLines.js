594b4c6e1b623086725c961f7f678672d6eab1e6
GemLavery <129361014+GemLavery@users.noreply.github.com>
2025-08-12 22:29:59 +1000

    Add files via upload

---
+++ spotifyLines.js
@@ -0,0 +1,10 @@
+function bandImagesTest() {
+    var element = document.body;
+    element.classList.toggle("bandImagesTestToggle");
+    
+ };
+
+ function bandImagesTest2() {
+    var element = document.body;
+    element.classList.toggle("bandImagesTest2Toggle");
+ }
